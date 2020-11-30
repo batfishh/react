85fb2043680c04dc39a8ce6421ab1b0f5ad20b3a
Ashwin Kumar Singh <ashwinkumarsingh@Ashwins-MacBook-Pro.local>
2020-11-30 16:03:22 +0530

    tryBranch

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
-
+import ApprovalCard from './Approvalcard'
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail author={faker.name.findName()} time="2PM" text="HELLO" image = {faker.image.animals()}/>
-            <CommentDetail author={faker.name.findName()} time="2PM" text="HELLO" image = {faker.image.animals()}/>
-            <CommentDetail author={faker.name.findName()} time="2PM" text="HELLO" image = {faker.image.animals()}/>
+            <ApprovalCard>
+                <CommentDetail author={faker.name.findName()} time="2PM" text={faker.hacker.phrase()} image = {faker.image.animals()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={faker.name.findName()} time="4PM" text={faker.hacker.phrase()} image = {faker.image.animals()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={faker.name.findName()} time="8PM" text={faker.hacker.phrase()} image = {faker.image.animals()}/>
+            </ApprovalCard>
         </div>
     )
 }
