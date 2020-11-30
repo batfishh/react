2962eb38feb8dd564e9c7050ed1ceb4dfa61b680
Ashwin Kumar Singh <ashwinkumarsingh@Ashwins-MacBook-Pro.local>
2020-11-30 13:20:36 +0530

    separateComponenent

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = ({author,text,time})=>{
+const CommentDetail = ({author,text,time,image})=>{
     return(
         <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.people()}/>
+                    <img alt="avatar" src={image}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 import CommentDetail from './CommentDetail'
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail author="ashwin" time="2PM" text="HELLO"/>
-            <CommentDetail author="rahul" time="2PM" text="HELLO"/>
-            <CommentDetail author="sam" time="2PM" text="HELLO"/>
+            <CommentDetail author={faker.name.findName()} time="2PM" text="HELLO" image = {faker.image.animals()}/>
+            <CommentDetail author={faker.name.findName()} time="2PM" text="HELLO" image = {faker.image.animals()}/>
+            <CommentDetail author={faker.name.findName()} time="2PM" text="HELLO" image = {faker.image.animals()}/>
         </div>
     )
 }
